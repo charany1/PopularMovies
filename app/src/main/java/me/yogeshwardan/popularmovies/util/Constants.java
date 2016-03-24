d059c36065da052923402d85fcc7a797f8be9ba3
Yogeshwar Dan Charan <charanyogeshwar@gmail.com>
2016-03-24 21:30:47 +0530

    Let's submit.

--- app/src/main/java/me/yogeshwardan/popularmovies/util/Constants.java
+++ app/src/main/java/me/yogeshwardan/popularmovies/util/Constants.java
@@ -5,7 +5,7 @@ package me.yogeshwardan.popularmovies.util;
 public interface Constants {
 
     public String REQUEST_BASE_URL = "http://api.themoviedb.org/";
-    public String API_KEY = "c07dd2aaa4c9e920c3b7bc4be62153f3";
+    public String API_KEY = "TMDB_API_KEY_HERE";
 
     public String IMAGE_PATH_BASE_URL = "http://image.tmdb.org/t/p/w185/";
     public String BACKDROP_IMAGE_PATH_BASE_URL = "http://image.tmdb.org/t/p/w342/";
